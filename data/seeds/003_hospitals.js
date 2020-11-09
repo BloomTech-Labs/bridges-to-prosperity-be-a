2e1c1bebf2a75c82ea3cb9727b6890c25412be29
Dominique K Kitchenakow <64430667+domikitchen@users.noreply.github.com>
2020-11-09 15:05:46 -0600

    thank you for pointing that out!

--- data/seeds/003_hospitals.js
+++ data/seeds/003_hospitals.js
@@ -6,8 +6,8 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('hospitals').insert([
         {hospital_id: 1, hospital_name: 'Kirehe', amenities_id: "1", lat: -2.267769, long: 30.653169},
-        {hospital_id: 1, hospital_name: 'Izimbya', amenities_id: "2", lat: -1.554162, long: 31.459114},
-        {hospital_id: 1, hospital_name: 'Nyamata', amenities_id: "3", lat: -2.138361, long: 30.079017}
+        {hospital_id: 2, hospital_name: 'Izimbya', amenities_id: "2", lat: -1.554162, long: 31.459114},
+        {hospital_id: 3, hospital_name: 'Nyamata', amenities_id: "3", lat: -2.138361, long: 30.079017}
       ]);
     });
 };

0135708fbc3835046b49a4c5cd064bf0452d63c5
Dominique K Kitchenakow <64430667+domikitchen@users.noreply.github.com>
2020-11-09 15:05:10 -0600

    switched the hospital id to suggested in the amenities table

--- data/migrations/20201109125611_create-hospital-and-amenities-tables.js
+++ data/migrations/20201109125611_create-hospital-and-amenities-tables.js
@@ -12,7 +12,7 @@ exports.up = function(knex) {
       .createTable('amenities', function (table) {
         table.integer('amenities_id');
         table.string('amenitie_name');
-        table.integer('hospital_id');
+        table.integer('closest_bridge_id');
         table.float('lat');
         table.float('long');
       })

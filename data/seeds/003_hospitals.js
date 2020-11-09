f2564fd2a9ad72cd2767e02c683a8ac25cd989c2
Dominique K Kitchenakow <64430667+domikitchen@users.noreply.github.com>
2020-11-09 15:21:17 -0600

    changed the amenities ids

--- data/seeds/003_hospitals.js
+++ data/seeds/003_hospitals.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('hospitals').insert([
-        {hospital_id: 1, hospital_name: 'Kirehe', amenities_id: "1", lat: -2.267769, long: 30.653169},
-        {hospital_id: 2, hospital_name: 'Izimbya', amenities_id: "2", lat: -1.554162, long: 31.459114},
-        {hospital_id: 3, hospital_name: 'Nyamata', amenities_id: "3", lat: -2.138361, long: 30.079017}
+        {hospital_id: 1, hospital_name: 'Kirehe', closest_bridge_id: "1", lat: -2.267769, long: 30.653169},
+        {hospital_id: 2, hospital_name: 'Izimbya', closest_bridge_id: "2", lat: -1.554162, long: 31.459114},
+        {hospital_id: 3, hospital_name: 'Nyamata', closest_bridge_id: "3", lat: -2.138361, long: 30.079017}
       ]);
     });
 };
